ede3eced1ceaf2458e2d7c2d54286bfeef6e487d
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-07-01 17:26:16 +0300

    Complete exercise "07_tempConversion"

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let convertedTemp;
+  convertedTemp = ((degree - 32) * (5 / 9));
+  if (!Number.isInteger(convertedTemp)) {
+    convertedTemp = +convertedTemp.toFixed(1);
+  }
+  return convertedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let convertedTemp;
+  convertedTemp = (degree * (9 / 5) + 32);
+  if (!Number.isInteger(convertedTemp)) {
+    convertedTemp = +convertedTemp.toFixed(1);
+  }
+  return convertedTemp;
 };
 
 // Do not edit below this line
