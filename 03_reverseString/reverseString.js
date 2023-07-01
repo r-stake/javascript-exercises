10fec691022bbd7571e17e7f02dd03b8bfff6935
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-07-01 11:01:27 +0300

    Finish exercise "03_reverseString"
    Originally planned to have separate paths in the function for scenarios with single and multiple word strings. After writing the function, i noticed that the part meant for single word strings also correctly reverses multiple word strings. Decided to keep the original solution as a reminder.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,22 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const wordArray = string.split(" ");
+  if (wordArray.length === 1) {
+    const charArray = string.split("");
+    charArray.reverse();
+    let reversedArray = charArray.join("");
+    return reversedArray;
+  }
+  let output = "";
+  let reversedArray = wordArray.reverse();
+  for (let i = 0; i < wordArray.length; i++) {
+    let currentWord = reversedArray[i];
+    const charArray = currentWord.split("");
+    charArray.reverse();
+    let currentWordReversed = charArray.join("")
+    output += `${currentWordReversed} `
+  }
+  reversedArray = output.trimEnd();
+  return reversedArray;
 };
 
 // Do not edit below this line
