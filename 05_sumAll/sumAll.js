ed56a5fe48cd9f7b4567c5a09b6e4addc1797d54
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-07-01 15:00:29 +0300

    Complete exercise "05_sumAll"

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+  let sum = 0;
+  let range = 0;
+  let i = 0;
+  if (typeof int1 != "number" || typeof int2 != "number" || int1 < 0 || int2 < 0) {
+    return "ERROR"
+  }
+  if (int1 > int2) {
+    i = int2;
+    range = int1;
+  } else {
+    i = int1
+    range = int2;
+  }
+  for (; i <= range; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
