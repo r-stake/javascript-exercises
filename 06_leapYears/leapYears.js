8676c17e70b805faa5dcb0fe14d1ab5f1696a719
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-07-01 17:01:01 +0300

    Add additional solution for exercise "06_leapYears"

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,18 +1,32 @@
 const leapYears = function(year) {
-  if (year % 4 === 0) {
-    if (year % 100 === 0) {
-        if (year % 400 === 0) {
-            return true;
-        } else {
-            return false;
-        }
-    } else {
-        return true;
-    }
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+    return true;
+  } else if (year % 4 === 0 && year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
   } else {
     return false;
   }
+
+//   if (year % 4 === 0) {
+//     if (year % 100 === 0) {
+//         if (year % 400 === 0) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     } else {
+//         return true;
+//     }
+//   } else {
+//     return false;
+//   }
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
