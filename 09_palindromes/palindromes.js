ac14a717e2b1138e40538dc6729c3e7154653829
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-07-16 15:19:00 +0300

    Complete exercise "09_palindromes"

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let modified = string.toLowerCase().split("").filter(char => /[a-z0-9]/.test(char)).join("");;
+    let stringForward = modified;
+    let stringBackward = modified.split("").reverse().join("");
 
+    if (stringForward === stringBackward) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
