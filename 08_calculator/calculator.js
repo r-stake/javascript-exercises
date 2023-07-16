711ad720f2e17d005860565b38f0ce918e06c532
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-07-16 14:40:39 +0300

    Complete exercise "08_calculator"

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for (let item of arr) {
+    if (item === undefined) {
+      item = 0;
+    }
+    sum += item;
+  }
+
+  return sum;
+
 };
 
-const multiply = function() {
-
+const multiply = function(...num) {
+  let result = 0;
+  for (let i = 0; i < num.length; i++) {
+    if (result === 0) {
+      result += num[i];
+    } else {
+      result *= num[i];
+    }
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+	if (!num) {
+    return result;
+  } else {
+    for (let i = num; i > 0; i--) {
+      result *= i;
+    }
+  }
+
+  return result;
 };
 
 // Do not edit below this line
