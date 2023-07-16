08c1021d32233871655ef52921368d044898cef1
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-07-16 16:58:32 +0300

    Complete exercise "12_findTheOldest"

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let howOld = []; 
+    howOld = arr.map(person => {
+        if (person.yearOfDeath === undefined) {
+            let age = new Date();
+            age = age.getFullYear() - person.yearOfBirth;
+            return {name: person.name, age: age};
+        } else {
+            let age = person.yearOfDeath - person.yearOfBirth;
+            return {name: person.name, age: age};
+        }
+    });
+    howOld.sort( (a, b) => b.age - a.age );
+    return howOld[0];
 };
 
 // Do not edit below this line
