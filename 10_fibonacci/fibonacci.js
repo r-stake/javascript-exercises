40ce11cbbeac2312b9f9b1d75bf4bb8bd5993f32
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-07-16 15:48:05 +0300

    Complete exercise "10_fibonacci"

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS"
+    let sequence = [];
+    for (let i = 0; i < +num; i++) {
+        let result = sequence[i - 1] + sequence[i - 2];
+        if (!result) {
+            sequence.push(1);
+        } else {
+            sequence.push(result);
+        }
+        
+    }
+    return sequence[+num - 1];
 };
 
 // Do not edit below this line
